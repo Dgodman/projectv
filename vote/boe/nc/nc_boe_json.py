4282cbb877758ceba4ac401574da6182b95cc39e
lemon <dgodman@gmail.com>
2017-07-20 11:14:39 -0400

    deleted comments

--- vote/boe/nc/nc_boe_json.py
+++ vote/boe/nc/nc_boe_json.py
@@ -14,46 +14,6 @@ def print_all_json(json_file):
     data = open_json(json_file)
     # loop board of elections
     for boe in data['nc-data']:
-        '''
-        # county name
-        county = boe['Name']
-
-        # physical address
-        physical_address = boe['PhysicalAddr1']
-        if not boe['PhysicalAddr2']:
-            physical_address = physical_address + ", {}".format(boe['PhysicalAddrCSZ'])
-        else:
-            physical_address = physical_address + " {}, {}".format(boe['PhysicalAddr2'], boe['PhysicalAddrCSZ'])
-
-        # mailing address
-        mailing_address = boe['MailingAddr1']
-        if not boe['MailingAddr2']:
-            mailing_address = mailing_address + ", {}".format(boe['MailingAddrCSZ'])
-        else:
-            mailing_address = mailing_address + " {}, {}".format(boe['MailingAddr2'], boe['MailingAddrCSZ'])
-        if mailing_address.split(' ', 1)[0] == "Same":
-            mailing_address = physical_address
-
-        # hours of operation
-        hours = boe['OfficeHours']
-
-        # phone #
-        phone = boe['OfficePhoneNum']
-        ext = boe['OfficePhoneNumExt']
-        if ext:
-            phone = phone + " x" + ext
-
-        # website url
-        url = boe['WebsiteAddr']
-
-        print(county + ":")
-        print("Physical Address: " + physical_address)
-        print("Mailing Address: " + mailing_address)
-        print("Phone #: " + phone)
-        print("Hours: " + hours)
-        print("Website: " + url)
-        print("\n")
-        '''
         boe_dict = parse_boe(boe)
         if boe_dict:
             print(boe_dict['county'] + ":")
