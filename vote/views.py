ab43163fd169c2d8c5024b70eb9b316bda860bbf
lemon <dgodman@gmail.com>
2017-07-21 15:42:03 -0400

    about page and set navbar class with views

--- vote/views.py
+++ vote/views.py
@@ -8,5 +8,16 @@ def index(request):
     return render(
         request,
         'vote/index.html',
-        {},
+        {'active_nav': 'home',},
+    )
+
+
+def about(request):
+    '''Home page of site'''
+
+    # render html
+    return render(
+        request,
+        'vote/about.html',
+        {'active_nav': 'about',},
     )
